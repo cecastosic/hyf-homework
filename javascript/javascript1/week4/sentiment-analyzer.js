6a91957ffee53711f32971d30b673ad0bdff1b54
Svetlana Stosic Milosavljevic <cecastosic@gmail.com>
2019-09-25 14:09:50 +0200

    sentiment analyzer function

---
+++ javascript/javascript1/week4/sentiment-analyzer.js
@@ -0,0 +1,26 @@
+const positive = ['mega', 'super', 'awesome', 'happy', 'beautiful', 'love', 'like', 'extra'];
+const negative = ['hate', 'boring', 'ugly', 'sad'];
+
+function getSentimentScore(sentence) {
+    const words = sentence.split(' ');
+    const positiveWords = [];
+    const negativeWords = [];
+    let score = 0;
+    for (let i = 0; i < words.length; i++) {
+        if (positive.includes(words[i])) {
+            positiveWords.push(words[i]);
+            score += 1;
+        } else if (negative.includes(words[i])) {
+            negativeWords.push(words[i]);
+            score -= 1;
+        }
+    }
+    return {score,positiveWords, negativeWords};
+}
+
+const sentimentScoreObject = getSentimentScore('I am mega super awesome happy');
+const sentimentScoreObject2 = getSentimentScore('I hate doing boring stuff');
+console.log(sentimentScoreObject); 
+console.log(sentimentScoreObject2); 
+
+
