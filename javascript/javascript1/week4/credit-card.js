d042af5c9d4e551a7e329b675792df6a53b49c82
Svetlana Stosic Milosavljevic <cecastosic@gmail.com>
2019-09-25 23:57:30 +0200

    format code

--- javascript/javascript1/week4/credit-card.js
+++ javascript/javascript1/week4/credit-card.js
@@ -1,12 +1,11 @@
 function formatCreditCardNumber(number) {
     const string = number.toString();
     const array = [];
-    for (let i = 0; i < string.length; i+=4) {
-        array.push(string.slice(i,i+4));
+    for (let i = 0; i < string.length; i += 4) {
+        array.push(string.slice(i, i + 4));
     }
     return array.join(' ');
 }
 
 const formattedCreditCardObject = formatCreditCardNumber(123456789);
 console.log(formattedCreditCardObject);
-
