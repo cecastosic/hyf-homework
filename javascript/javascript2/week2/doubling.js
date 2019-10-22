f930e9f92365a9f350375fc78ef0894ce30873db
Svetlana Stosic Milosavljevic <cecastosic@gmail.com>
2019-10-22 11:10:49 +0200

    refactoring

--- javascript/javascript2/week2/doubling.js
+++ javascript/javascript2/week2/doubling.js
@@ -11,7 +11,7 @@ const numbers = [1, 2, 3, 4];
 //Rewrite the above program using map and filter don't forget to use arrow functions.
 
 const newNumbers = numbers
-    .filter(number => number % 2 !== 0 ? true : false)
+    .filter(number => number % 2 !== 0)
     .map(number => number * 2);
 
 console.log("The doubled numbers are", newNumbers); // [2, 6]
