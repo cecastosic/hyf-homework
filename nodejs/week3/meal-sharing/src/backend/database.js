88cad8b662b71cd99681bbdaac0c5c117c108842
Svetlana Stosic Milosavljevic <cecastosic@gmail.com>
2020-01-13 21:24:51 +0100

    using createPool

--- nodejs/week3/meal-sharing/src/backend/database.js
+++ nodejs/week3/meal-sharing/src/backend/database.js
@@ -4,8 +4,7 @@ const mysql = require("mysql");
 
 require("dotenv").config();
 
-//const pool = mysql.createPool({
-const pool = mysql.createConnection({
+const pool = mysql.createPool({
   connectionLimit: 10,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -15,20 +14,20 @@ const pool = mysql.createConnection({
   multipleStatements: true
 });
 
-// pool.getConnection((err, connection) => {
-//   if (err) {
-//     if (err.code === "PROTOCOL_CONNECTION_LOST") {
-//       console.error("Database connection was closed.");
-//     }
-//     if (err.code === "ER_CON_COUNT_ERROR") {
-//       console.error("Database has too many connections.");
-//     }
-//     if (err.code === "ECONNREFUSED") {
-//       console.error("Database connection was refused.");
-//     }
-//   }
-//   if (connection) connection.release();
-//   return;
-// });
+ pool.getConnection((err, connection) => {
+   if (err) {
+     if (err.code === "PROTOCOL_CONNECTION_LOST") {
+       console.error("Database connection was closed.");
+     }
+    if (err.code === "ER_CON_COUNT_ERROR") {
+      console.error("Database has too many connections.");
+    }
+    if (err.code === "ECONNREFUSED") {
+      console.error("Database connection was refused.");
+    }
+  }
+  if (connection) connection.release();
+  return;
+});
 
 module.exports = pool;
