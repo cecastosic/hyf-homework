af1ebef628f4d1ff9cf7f0b80f089545d004c52b
Svetlana Stosic Milosavljevic <cecastosic@gmail.com>
2020-02-04 23:28:54 +0100

    fixing counter

--- react/week2/todo/src/App.js
+++ react/week2/todo/src/App.js
@@ -13,10 +13,12 @@ function Header() {
 function Counter() {
   // timer that counts how long time a users has spent on the website
   const [count, setCount] = useState(0);
+  
   useEffect(() => {
-    setTimeout(() => {
+    let timer = setTimeout(() => {
       setCount(prev => prev + 1);
     }, 1000);
+    return (() => clearTimeout(timer)); 
   });
 
   return <p>You have used {count} seconds on this website</p>;
