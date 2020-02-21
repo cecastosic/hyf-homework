0dd7fa931e4469e567e2c5425f0b4f6b60aa2ae4
Svetlana Stosic Milosavljevic <cecastosic@gmail.com>
2020-02-21 15:35:21 +0100

    refactoring - prettier and removing console.log

--- react/week4/gitsearch/src/components/User.js
+++ react/week4/gitsearch/src/components/User.js
@@ -1,21 +1,21 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import UserDetails from "./UserDetails";
 
 function User({ user }) {
   const [details, setDetails] = useState(false);
-  
-  const showDetails = (event) => {
+
+  const showDetails = event => {
     event.preventDefault();
     setDetails(!details);
-  }
+  };
 
   return (
     <li>
-      <a href={user.url} onClick={(event) => showDetails(event)}>
+      <a href={user.url} onClick={event => showDetails(event)}>
         <img src={user.avatar_url} alt={user.login} />
         <span>{user.login}</span>
       </a>
-      { details && <UserDetails id={user.id} user={user}/>}
+      {details && <UserDetails id={user.id} user={user} />}
     </li>
   );
 }
--- react/week4/gitsearch/src/components/UserDetails.js
+++ react/week4/gitsearch/src/components/UserDetails.js
@@ -1,33 +1,32 @@
 import React, { useState, useEffect } from "react";
 
-function UserDetails({id, user}) {
-    const [details, setDetails] = useState("");
+function UserDetails({ id, user }) {
+  const [details, setDetails] = useState("");
 
-    const fetchDetailsData = async () => {
+  const fetchDetailsData = async () => {
     const url = `https://api.github.com/users/${user.login}`;
     const details = await fetch(url)
-                 .then(data => data.json())
-                 .catch(error => console.log(error));
-                 console.log(details);
-        setDetails(details);
-    }
-    useEffect(() => {
-        fetchDetailsData();
-      }, []);
+      .then(data => data.json())
+      .catch(error => console.log(error));
+    setDetails(details);
+  };
+  useEffect(() => {
+    fetchDetailsData();
+  }, []);
 
-    return (
-        <div className="details">
-            <p className="bold">{details.name}</p>
-            <p>{details.blog}</p>
-            <p>{details.bio}</p>
-            <p>{details.location}</p>
-            <div className="section">
-                <p>Public repos: {details.public_repos}</p>
-                <p>Followers: {details.followers}</p>
-                <p>Following: {details.following}</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="details">
+      <p className="bold">{details.name}</p>
+      <p>{details.blog}</p>
+      <p>{details.bio}</p>
+      <p>{details.location}</p>
+      <div className="section">
+        <p>Public repos: {details.public_repos}</p>
+        <p>Followers: {details.followers}</p>
+        <p>Following: {details.following}</p>
+      </div>
+    </div>
+  );
 }
 
 export default UserDetails;
